a8e7e69eb13c61ec7ab987e93f4deafd13a089a9
Sung-Hun Bae <sunghun.bae@gmail.com>
2022-06-21 11:23:51 -0400

    public

--- batch-convert-pdb-to-maegz.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-for pdb in $( ls *.pdb )
-do
-  maegz=${pdb::-4}.maegz
-  echo $maegz
-  $SCHRODINGER/utilities/structconvert $pdb $maegz
-done
--- batch-structcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for aso in $( ls init/MDN*.mae )
-do
-  prefix=$( basename ${aso} .mae )
-  echo $prefix
-  $SCHRODINGER/utilities/structcat -imae 2qk9-apo-chain-c.maegz $aso -omae init/2qk9-${prefix}.maegz
-done
